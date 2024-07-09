54264111e8ac70f150aa8a9623753161b37c84f6
Adan Salman <adansalman73@gmail.com>
2024-07-09 15:44:00 +0500

    changes

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
-import { HttpExceptionFilter } from './http-exception-filter';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(8000);
 }
 bootstrap();
