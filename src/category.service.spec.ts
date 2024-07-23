a00d6f081ee3cf1b2c9f568c01cad135be6595fb
Adan Salman <adansalman73@gmail.com>
2024-07-23 12:09:04 +0500

    category service unit test cases

---
+++ src/category.service.spec.ts
@@ -0,0 +1,93 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { CreateCategoryDto } from 'src/dto/category-dto/create-category.dto';
+import { UpdateCategoryDto } from 'src/dto/category-dto/update-category.dto';
+import { Category, Users } from './entities';
+import { CategoryService } from './services/category.service';
+
+describe('CategoryService', () => {
+  let service: CategoryService;
+
+  const mockCategoryRepository = {
+    find: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+    update: jest.fn(),
+    findOneBy: jest.fn(),
+    delete: jest.fn(),
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        CategoryService,
+        {
+          provide: getRepositoryToken(Category),
+          useValue: mockCategoryRepository,
+        },
+      ],
+    }).compile();
+
+    service = module.get<CategoryService>(CategoryService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should return an array of categories', async () => {
+    const categories: Category[] = [
+      {
+        id: 1,
+        category_name: 'Work',
+        user_id: 1,
+        user: new Users(),
+        tasks: [],
+      } as Category,
+    ];
+    mockCategoryRepository.find.mockResolvedValue(categories);
+
+    expect(await service.getAllCategorys()).toEqual(categories);
+  });
+  it('should create and return a new category', async () => {
+    const createCategoryDto: CreateCategoryDto = {
+      category_name: 'Personal',
+      user_id: 1,
+    };
+    const category: Category = {
+      id: 1,
+      ...createCategoryDto,
+      user: new Users(),
+      tasks: [],
+    } as Category;
+    mockCategoryRepository.create.mockReturnValue(category);
+    mockCategoryRepository.save.mockResolvedValue(category);
+
+    expect(await service.addNewCategory(createCategoryDto)).toEqual(category);
+  });
+
+  it('should update and return the category', async () => {
+    const updateCategoryDto: UpdateCategoryDto = {
+      category_name: 'Updated Category',
+      user_id: 1,
+    };
+    const category: Category = {
+      id: 1,
+      ...updateCategoryDto,
+      user: new Users(),
+      tasks: [],
+    } as Category;
+    mockCategoryRepository.update.mockResolvedValue(null);
+    mockCategoryRepository.findOneBy.mockResolvedValue(category);
+
+    expect(await service.updateCategory(1, updateCategoryDto)).toEqual(
+      category,
+    );
+  });
+
+  it('should delete the category', async () => {
+    mockCategoryRepository.delete.mockResolvedValue(null);
+
+    expect(await service.deleteCategory(1)).toBeNull();
+  });
+});
--- src/user.service.spec.ts
+++ src/user.service.spec.ts
@@ -1,6 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './services/user.service';
-import { Repository } from 'typeorm';
 import { Users } from './entities/create-user.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { CreateUserDto } from 'src/dto/user-dto/create-user.dto';
@@ -8,9 +7,8 @@ import { UpdateUserDto } from 'src/dto/user-dto/update-user.dto';
 
 describe('UserService', () => {
   let service: UserService;
-  let repo: Repository<Users>;
 
-  const mockUserServices = {
+  const mockUserRepository = {
     find: jest.fn(),
     create: jest.fn(),
     save: jest.fn(),
@@ -22,72 +20,58 @@ describe('UserService', () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         UserService,
+        {
+          provide: getRepositoryToken(Users),
+          useValue: mockUserRepository,
+        },
       ],
-    }).overrideProvider(UserService).useValue(mockUserServices).compile();
+    }).compile();
 
     service = module.get<UserService>(UserService);
-    //repo = module.get<new Repository<Users>(Users);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 
-  describe('getAllUsers', () => {
-    it('should return an array of users', async () => {
-      const usersArray = [
-        { id: 1, username: 'testuser', email: 'test@example.com' },
-      ];
-      mockUserServices.find.mockResolvedValue(usersArray);
+  it('should return an array of users', async () => {
+    const usersArray = [
+      { id: 1, username: 'testuser', email: 'test@example.com' },
+    ];
 
-      expect(await service.getAllUsers()).toBe(usersArray);
-    });
+    mockUserRepository.find.mockResolvedValue(usersArray);
+    expect(await service.getAllUsers()).toBe(usersArray);
   });
 
-  describe('addNewUser', () => {
-    it('should successfully add a new user', async () => {
-      const createUserDto: CreateUserDto = {
-        username: 'newuser',
-        email: 'newuser@example.com',
-        password: 'password',
-      };
-      const newUser = {
-        id: 2,
-        ...createUserDto,
-      };
-      mockUserServices.create.mockReturnValue(newUser);
-      mockUserServices.save.mockResolvedValue(newUser);
+  it('should successfully add a new user', async () => {
+    const createUserDto: CreateUserDto = {
+      username: 'newuser',
+      email: 'newuser@example.com',
+      password: 'password',
+    };
+    const newUser = {
+      id: 2,
+      createUserDto,
+    };
+    mockUserRepository.create.mockReturnValue(newUser);
+    mockUserRepository.save.mockResolvedValue(newUser);
 
-      expect(await service.addNewUser(createUserDto)).toBe(newUser);
-    });
-
-    it('should throw an error if user cannot be saved', async () => {
-      const createUserDto: CreateUserDto = {
-        username: 'newuser',
-        email: 'newuser@example.com',
-        password: 'password',
-      };
-      mockUserServices.save.mockRejectedValue(new Error('Could not save user'));
-
-      await expect(service.addNewUser(createUserDto)).rejects.toThrow('Could not save user');
-    });
+    expect(await service.addNewUser(createUserDto)).toBe(newUser);
   });
 
-  describe('updateUser', () => {
-    it('should successfully update a user', async () => {
-      const updateUserDto: UpdateUserDto = {
-        username: 'updateduser',
-        email: 'updateduser@example.com',
-        password: ''
-      };
-      const updatedUser = {
-        id: 1,
-        ...updateUserDto,
-      };
-      mockUserServices.update.mockResolvedValue(undefined);
-      mockUserServices.findOneBy.mockResolvedValue(updatedUser);
+  it('should successfully update a user', async () => {
+    const updateUserDto: UpdateUserDto = {
+      username: 'updateduser',
+      email: 'updateduser@example.com',
+      password: 'updatedpw',
+    };
+    const updatedUser = {
+      id: 1,
+      updateUserDto,
+    };
+    mockUserRepository.update.mockResolvedValue(undefined);
+    mockUserRepository.findOneBy.mockResolvedValue(updatedUser);
 
-      expect(await service.updateUser(1, updateUserDto)).toBe(updatedUser);
-    });
+    expect(await service.updateUser(1, updateUserDto)).toBe(updatedUser);
   });
 });
